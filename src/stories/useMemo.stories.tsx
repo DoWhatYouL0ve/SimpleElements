67d0b65e89a0dd1d29af880d012d607424cd6396
DoWhatYouL0ve <uladzimirpryshchapionak@protonmail.com>
2021-04-03 01:14:21 +0200

    adding useMemo with React.memo

---
+++ src/stories/useMemo.stories.tsx
@@ -0,0 +1,79 @@
+import React, {useMemo, useState} from "react";
+import {Meta} from "@storybook/react/types-6-0";
+
+
+export default {
+    title: 'My components/useMemo demo',
+} as Meta;
+
+
+// count factorial
+export const DifficultcountingExample = () => {
+
+    const [a, setA] = useState<number>(0)
+    const [b, setB] = useState<number>(0)
+
+    let resultA = 1
+    let resultB = 1
+
+    resultA = useMemo( () => {
+        let tempResultA = 1
+        for(let i = 1; i <= a; i++) {
+            let fake = 0
+            while (fake < 10000000) {
+                fake++
+                let fakeValue = Math.random()
+            }
+            tempResultA *= i
+        }
+        return tempResultA
+    }, [a])
+
+
+    for(let i = 1; i <= b; i++) {
+        resultB *= i
+    }
+
+    return <>
+        <input type="text" value={a} onChange={(e) => setA(+e.currentTarget.value)}/>
+        <input type="text" value={b} onChange={(e) => setB(+e.currentTarget.value)}/>
+        <hr/>
+        <div>
+            Result for a: {resultA}
+        </div>
+        <div>
+            Result for b: {resultB}
+        </div>
+    </>
+}
+
+const UsersSecret = (props: {user: Array<string>}) => {
+    console.log('UsersSecret')
+    return <div>{
+        props.user.map( (u,i) => <div key={i}>{u}</div>)
+    }</div>
+}
+
+const Users = React.memo(UsersSecret)
+
+export const HelpsToReactMemo = () => {
+    console.log('HelpsToReactMemo')
+    const [counter, setCounter] = useState(0)
+    const [user, setUser] = useState(['Dimych', 'Valera', 'Artem'])
+    const onClick = () => {
+        setCounter(counter+1)
+    }
+
+    const addUser = () => {
+        const newUser = [...user, ('Vova ' + new Date().getTime())]
+        setUser(newUser)
+    }
+
+    let newArray = useMemo( () => user.filter( u => u.toLowerCase().indexOf('a') > -1 ), [user])
+    return <>
+        <button onClick={onClick}>+</button>
+        <button onClick={addUser}>add user</button>
+        {counter}
+        <Users user={newArray} />
+    </>
+}
